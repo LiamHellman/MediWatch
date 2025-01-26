62f804d230e51c5e5a892e6dfbaea9390bf9e028
AariAlma <aariyan@gmail.com>
2025-01-25 23:39:12 -0500

    merging my and yasins stuff

---
+++ react-app/src/App.tsx
@@ -0,0 +1,16 @@
+
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = [
+          'New York',
+          'San Fransisco',
+          'Tokyo',
+          'London',
+          'Paris'
+      ]; 
+
+  return <div><ListGroup items={items} heading="Cities" /></div>
+}
+
+export default App;
